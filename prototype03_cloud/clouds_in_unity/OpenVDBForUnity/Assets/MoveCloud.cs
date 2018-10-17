6b0d37a9820982936657ecee65a48859706c0d38
Nathan Koch <nathanrkoch@gmail.com>
2018-10-17 12:05:25 -0400

    added unity prototype

---
+++ prototype03_cloud/clouds_in_unity/OpenVDBForUnity/Assets/MoveCloud.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCloud : MonoBehaviour
+{
+    public float speed = 3.0f;
+    public float rotationSpeed = 4.0f;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(new Vector3(Time.deltaTime * speed, 0, 0));
+        transform.Rotate(Vector3.up, Time.deltaTime * rotationSpeed);
+    }
+}
---
+++ prototype03_cloud/clouds_in_unity/OpenVDBForUnity/Assets/SpawnCloud.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnCloud : MonoBehaviour
+{
+
+    public float initialDelay = 1.0f;
+    public float rate = 3.0f;
+    public Vector3 spawnLocation = new Vector3(-38, 0, 0);
+    public GameObject[] clouds;
+
+    // Use this for initialization
+    void Start()
+    {
+        InvokeRepeating("SpawnNewCloud", initialDelay, rate);
+    }
+
+    void SpawnNewCloud()
+    {
+        Vector3 newLocation = new Vector3(spawnLocation.x, Random.Range(-10, 10), Random.Range(-2, 2));
+        GameObject cloud = clouds[Random.Range(0, clouds.Length)];
+        GameObject obj = Instantiate(cloud, newLocation, Quaternion.identity);
+        MoveCloud mc = obj.GetComponent<MoveCloud>();
+        mc.speed = Random.Range(1.0f, 5.0f);
+        mc.rotationSpeed = Random.Range(1.0f, 3.0f);
+    }
+}
